d6e8bed8fb93005743584fd32810a3ae185b4d69
andreef1981 <25109174+andreef1981@users.noreply.github.com>
2018-08-17 13:23:43 -1000

    helper added

--- ddLinearity.py
+++ ddLinearity.py
@@ -9,10 +9,9 @@ Created on Tue Aug  7 12:11:00 2018
 import numpy as np
 import matplotlib.pyplot as plt
 from cnPipeline import *
+from helperFunctions import *
 
-def linearity(data,
-         linearityCoefficients,
-         dataTime):
+def linearity(data, threshold=66000.):
   """
   Returns a linearised ramp of the CryoNIRSP H2RG. Assuming only a quadratic 
   term is removed
@@ -21,13 +20,8 @@ def linearity(data,
     ----------
     data : (#NDRs, 2048, 2048) ndarray, uint16
         3D data cube that needs to be linearity corrected.
-    linearityCoefficients : (3, 2048, 2048) ndarray, float32
-        3D data cube that contains the parameters to a quadratic curve 
-        y = a*t**2 + b*t + c . Where the [0,:,:] contains the quadratic term.
-        Units of [b]= ADU/second, [a]=ADU/second**2
-    dataTime : (#NDRs,) ndarray, float32
-        vector containing the timestamps for the NDRs up the ramp.
-        Units [dataTime] = seconds    
+    threshold : float
+        threshold for the quadratic fit. Everything above will be excluded.
 
     Returns
     -------
@@ -36,8 +30,6 @@ def linearity(data,
 
     Raises
     ------
-    AssertationError
-        If the shapes of the input arrays do not match.
 
     Notes
     -----
@@ -58,25 +50,35 @@ def linearity(data,
     >>> data = np.tile(val[:,None,None],(1,10,10))
     >>> res = linearity(data,c,t)
    """
+  if len(data.shape) == 3:
+    # only a single ramp is provided but matrix quadfit expects another axis
+    data = np.expand_dims(data, axis=0)
+    
+  # in this case we use the NDR number as our 'time' axis
+  dataTime = np.arange(data.shape[1])
+  # fit quadratic to data
+  a,b,c = matrixQuadfit(data,threshold=90,ignoreRef=False)
   
   # Use numpy's multiply capability to to the multiplication along the right axis
-  nonLinear = np.multiply(np.expand_dims(linearityCoefficients[0,:,:],axis=0),
-                          dataTime[:,None,None]**2.)
+#  nonLinear = np.multiply(np.expand_dims(linearityCoefficients[0,:,:],axis=0),
+#                          dataTime[:,None,None]**2.)
+  nonLinear = np.multiply(c,dataTime[:,None,None]**2.)
   
-  linearityCorrected = data - nonLinear
+  linearityCorrected = np.squeeze(data) - nonLinear
   
   return linearityCorrected
 
-#t = np.arange(12,dtype=np.float32)
-#c = np.zeros((3,10,10),dtype=np.float32)
-#c[0,:,:] = .3
-#c[1,:,:] = 2
-#c[2,:,:] = 1
-#val = np.polyval([.3,2,1],t)
-#lin = np.polyval([2,1],t)
-#data = np.tile(val[:,None,None],(1,10,10))
-#res = linearity(data,c,t)
+t = np.arange(12,dtype=np.float32)
+c = np.zeros((3,10,10),dtype=np.float32)
+c[0,:,:] = .3
+c[1,:,:] = 2
+c[2,:,:] = 1
+val = np.polyval([.3,2,1],t)
+val[2] = 100
+lin = np.polyval([2,1],t)
+data = np.tile(val[:,None,None],(1,10,10))
+res = linearity(data)
 #
-#fig, ax=plt.subplots()
-#ax.plot(t,val,t,lin, 'r', t, res[:,0,0],'g')
-#plt.show()
+fig, ax=plt.subplots()
+ax.plot(t,val,t,lin, 'r', t, res[:,0,0],'g')
+plt.show()
---
+++ helperFunctions.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(Andre Fehlmann)s
+
+helper library
+"""
+
+import numpy as np  # Fast numeric arrays/matrices (FITS files imported as numpy matrices)
+
+def inversematrices(data):
+    # From Kathleen Tatem
+    N = len(data[0,:,0,0])
+    dim = (N, 6)
+    
+    #initial inverse array that is N by 6
+    Cinv = np.zeros(dim)
+    
+    times = np.arange(N)
+    for t in times:
+        #create inverse matrix depending on the length of the ramp 
+        t0 = np.ones(t+1)
+        t1 = np.arange(t+1)
+        t2 = t1**2
+        
+        #the Cij matrix is a symmetric matrix, so there are only 6 elements we need to calculate
+        c00 = t0.sum(axis=0)
+        c01 = t1.sum(axis=0)
+        c02 = t2.sum(axis=0)
+        c11 = np.dot(t1, t1)
+        c12 = np.dot(t1, t2)
+        c22 = np.dot(t2, t2)
+        
+        #fill the Cij matrix
+        Cij = np.zeros((3, 3))
+        Cij[0, 0] = c00
+        Cij[0, 1] = c01
+        Cij[1, 0] = c01
+        Cij[0, 2] = c02
+        Cij[2, 0] = c02
+        Cij[1, 1] = c11
+        Cij[1, 2] = c12
+        Cij[2, 1] = c12
+        Cij[2, 2] = c22        
+        
+        #take the inverse of the Cij matrix
+        #creates a 3x3 array that is the inverse of Cij (when t = 2, 1, or 0, the matrix is not invertible)
+        c = np.zeros(6)
+        if t > 2:
+            invCji = np.linalg.inv(Cij)
+            c[0] = invCji[0,0]
+            c[1] = invCji[1,1]
+            c[2] = invCji[2,2]
+            c[3] = invCji[0,1]
+            c[4] = invCji[0,2]
+            c[5] = invCji[1,2]
+        
+            Cinv[t] = c
+                
+    return Cinv    
+
+def matrixQuadfit(data,threshold=66000,ignoreRef=False):  
+    # From Kathleen Tatem
+    #don't use the reference pixels in calculations
+    if ignoreRef:
+      data = data[:, :, 4:-4, 4:-4]
+
+#    threshold = 66000#45000 #for slow mode
+    
+    #put a zero everywhere in the 4D data cube that the intensity is above or equal to threshold 
+    #doesn't account for when data goes below threshold again after saturating
+    unsatdata = np.where(data <= threshold, data, 0) 
+    #make a mask data set, where data above threshold = 0, and data below threshold = 1
+    mask = np.where(unsatdata == 0, unsatdata, 1) 
+    #1-D array of the frame times from 0 to N 
+    t = np.arange(len(data[0,:,0,0])) 
+    #read in the inverse matrix elements given the number of frames in the data set
+    Cinv = inversematrices(data)
+   
+    #create a for loop over the frame numbers, starting from frame 1
+    bij = np.zeros((len(data[:,0,0,0]), len(data[0,0,:,0]), len(data[0,0,0,:])))
+    cij = np.zeros((len(data[:,0,0,0]), len(data[0,0,:,0]), len(data[0,0,0,:]))) 
+    lastUnsatFrame = np.zeros((len(data[:,0,0,0]), len(data[0,:,0,0]), len(data[0,0,:,0]), len(data[0,0,0,:])))
+    invCijperpix = np.zeros((6, len(data[:,0,0,0]), len(data[0,0,:,0]), len(data[0,0,0,:])))
+    for i in t[1:]:
+        #makes sure that pixels that cross the threshold say out of our calculations even if they go under threshold again
+        mask[:,i] = mask[:,i]*mask[:, i-1]        
+        unsatdata[:,i] = unsatdata[:,i]*mask[:, i-1]
+        #dot the data with time vectors of order 1, and 2
+        bij = (unsatdata[:,i]*i) + bij
+        cij = (unsatdata[:,i]*(i**2)) + cij
+        #put a one at the frame time when the pixel was last below threshold
+        lastUnsatFrame[:,i-1] = np.abs(mask[:,i] - mask[:, i-1]) #make sure it is full of 1s and 0s not -1s and 0s
+        #if a pixel never saturated, put a 1 in the in the last frame of the lasUnsatFrame
+        lastUnsatFrame[:, t[-1]] = mask[:, t[-1]]
+
+    #find the correct inverse matrix elements to use for each pixel (depending on time that the pixel was last below saturation)
+    indMatElements = np.arange(6)
+    for i in t:
+        for j in indMatElements:
+            invCijperpix[j] = (lastUnsatFrame[:,i]*(Cinv[i, j])) + invCijperpix[j]
+
+    #dot the data with the time vector of order 0
+    aij = unsatdata.sum(axis=1)
+
+    #create an array of the polynomial fit coefficients for each pixel
+    coefs = np.zeros((3, len(data[:,0,0,0]), len(data[0,0,:,0]), len(data[0,0,0,:])))
+    coefs[0] = (invCijperpix[0]*aij) + (invCijperpix[3]*bij) + (invCijperpix[4]*cij)
+    coefs[1] = (invCijperpix[3]*aij) + (invCijperpix[1]*bij) + (invCijperpix[5]*cij)
+    coefs[2] = (invCijperpix[4]*aij) + (invCijperpix[5]*bij) + (invCijperpix[2]*cij)
+        
+    return coefs[0], coefs[1], coefs[2]
