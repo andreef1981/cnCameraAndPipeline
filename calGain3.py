1c238355696e2784964813b6d67705e179d42880
andreef1981 <25109174+andreef1981@users.noreply.github.com>
2018-08-21 08:27:50 -1000

    added wavecal and gain

---
+++ calGain3.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: Andre Fehlmann (afehlmann@nso.edu)
+"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: Andre Fehlmann (afehlmann@nso.edu)
+
+Revision history
+----------------
+15 August 2018:
+    Removed instrument dark subtractiona as we only use background dark stand
+    alone and thus the instrument dark has to be contained. Andre
+    
+"""
+import numpy as np
+from astropy.io import fits
+from cnPipeline import *
+from ddLinearity import *
+
+def masterGain(data,
+               threshold=None,
+               mode=None,
+               writeToFile=False,
+               path=None,
+               sequenceName=None,
+               fileFormat='fits'):
+  
+  """
+  Returns the gain table for the CryoNIRSP H2RG.
+  
+   Parameters
+    ----------
+    data : (#ramps, #NDRs, 2048, 2048) ndarray, float32
+        4D data cube that will be averaged.
+    writeToFile : bolean, optional, default=False
+        writing to fits files for testing
+    path: string
+        directory to which optinal fits files will be written to
+    sequenceName: string
+        name of sequence to which optinal fits files will be written to
+    fileFormat: string ('fits' | 'arr')
+        writing fits files or little endian binary arrays
+
+    Returns
+    -------
+    gainTable : (2048, 2048) ndarray, float32
+        gain table for multiplicative gain correction
+
+    Raises
+    ------
+    
+    Notes
+    -----
+    
+    CSS uses little endian (16 or 32 bits). BTD converts to big endian for 
+    network transport. All simulated data should be little endian.
+    
+    Examples
+    --------
+    These are written in doctest format, and should illustrate how to
+    use the function.
+
+    >>> data = np.zeros((3,5,10,10),dtype='uint16')+6
+    >>> masterInstrumentDark = np.zeros((5,10,10),dtype='float32')+3
+    >>> masterBackgroundDark = masterBackgroundDark(data, masterInstrumentDark,
+                                              writeToFits=True,
+                                              path='data/backgroundDark/',
+                                              sequenceName='masterBackgroundDark')
+   """
+  
+  
+  #TODO: reference pixel correction should be done prior to averaging. Need to check when and if to do it.
+  
+  if len(data.shape) == 3:
+    # only a single ramp is provided but matrix quadfit expects another axis
+    data = np.expand_dims(data, axis=0)
+    
+  # matrixQuadfit needs float data to work properly
+  data = np.float32(data)
+    
+  # in this case we use the NDR number as our 'time' axis
+  dataTime = np.arange(data.shape[1])
+  
+  # fit quadratic to data
+  a,b,c = matrixQuadfit(data,threshold=threshold,mode=mode,ignoreRef=False)
+  # Use numpy's multiply capability to to the multiplication along the right axis
+#  nonLinear = np.multiply(np.expand_dims(linearityCoefficients[0,:,:],axis=0),
+#                          dataTime[:,None,None]**2.)
+  
+  temp = np.mean(b,axis=0)
+  gainTable = np.mean(temp)/temp
+  # for test purposes lets write the files to fits
+  if writeToFile:
+    if fileFormat == 'fits':
+      hdu = fits.PrimaryHDU(gainTable)
+      hdu.writeto(path+sequenceName+'.fits',overwrite=True)
+    else:
+      gainTable.tofile(path+sequenceName+'.arr',sep="")
+  
+  return gainTable
+
+
+#a=cnH2rgRamps("data/gain/simGainVariation*",readMode="SLOW",subArray=None,verbose=True)
+#data = np.squeeze(a.read(dtype=np.uint16))
+#linData=np.zeros(data.shape)
+#for i in range(3):
+#  linData[i,:,:,:] = linearity(data[i,:,:,:], threshold=0, mode="SLOW")
+#c= masterGain(data, threshold=0, mode="SLOW", writeToFile=True, path='data/gain/',
+#                     sequenceName='gainTable',fileFormat='fits')
+
+#fig, ax=plt.subplots()
+#plt.imshow(data[0,5,:,:])
+#
+#fig, ax=plt.subplots()
+#plt.imshow(data[0,5,:,:]*c)
+#plt.show()
---
+++ ddWavelength.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: Andre Fehlmann (afehlmann@nso.edu)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from cnPipeline import *
+#from helperFunctions import *
+#from astropy.io import fits  # Reading/writing FITS data
+
+def wavelength(data,
+         waveCal,
+         beamMapping):
+  """
+  Returns an array containing the wavelength for each pixel CryoNIRSP H2RG. 
+  I keep the beamMapping information from ALIGN tasks here so we could
+  use it to do the mapping of the two beams. This might either be done before
+  or after demodulation.
+  
+   Parameters
+    ----------
+    data : (#NDRs, 2048, 2048) ndarray, uint16
+        3D data cube that needs to be mapped corrected.
+    waveCal : (2048, 2048) ndarray, float32
+        output from WAVECAL calibration task describing what the wavelength
+        at each pixel is
+    beamMapping : (2, 2048, 2048) ndarray, float32
+        output from ALIGN3 calibration task
+        information describing how left and right side of beam match. 
+
+    Returns
+    -------
+    waveVector : (2048,) ndarray, float32
+        vector containing the wavelength at each pixel
+
+    Raises
+    ------
+
+    Notes
+    -----
+    Potentially we might want to remove curvature of lines in this and also do
+    some beam mapping
+    
+    Examples
+    --------
+    These are written in doctest format, and should illustrate how to
+    use the function.
+
+    >>> 
+   """
+  
+  # broadcasting does the right thing by multiplying all frames with the gain
+  # table
+  waveVector = waveCal[1024,:]
+  
+  return waveVector
+
+# sample code to run dark plugin
+#a=cnH2rgRamps("data/gain/simGain*",readMode="SLOW",subArray=None,verbose=True)
+#data = np.squeeze(a.read(dtype=np.uint16))
+#
+#gainTable = in_im = fits.open("data/gain/gainTable.fits")[0].data.astype(np.float32)
+#b = gain(data,gainTable)
+#
+#fig, ax=plt.subplots()
+#plt.imshow(data[1,1,:,:])
+#
+#fig, ax=plt.subplots()
+#plt.imshow(b[1,1,:,:])
+#plt.show()
